75d3172c99aedb11dc872662a22644389b6133d9
Tim <nosite@users.noreply.github.com>
2015-01-07 16:53:46 +0100

    added link to panel

--- src/admin/language/english/module/sendcloud.php
+++ src/admin/language/english/module/sendcloud.php
@@ -13,7 +13,7 @@ $_['msg_no_api_settings'] = 'Error: No api public and/or secret key found.';
 $_['msg_process_orders'] = "Error: Can't process order id(s): ";
 $_['msg_api_error_reason'] = "Due to the following error: ";
 $_['msg_order_no_shipping'] = 'Error: No shipping details for order(s): ';
-$_['msg_success'] = 'Orders successfully sent to SendCloud.';
+$_['msg_success'] = 'Orders successfully sent to SendCloud. <a href="https://panel.sendcloud.nl" target="_blank">Go to SendCloud</a>';
 $_['log_message'] = 'Transported to SendCloud.';
 
 $_['error_no_orders'] = 'Error: No orders selected.';
